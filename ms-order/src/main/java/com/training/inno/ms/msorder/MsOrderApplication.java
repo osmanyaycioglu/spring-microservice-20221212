33787e88250174fcd1cfc04c4d7f127d65d51f4b
osman <osman@osy.com>
2022-12-13 16:00:25 +0300

    day 2 3

---
+++ ms-order/src/main/java/com/training/inno/ms/msorder/MsOrderApplication.java
@@ -0,0 +1,14 @@
+package com.training.inno.ms.msorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsOrderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsOrderApplication.class,
+                              args);
+    }
+
+}
---
+++ ms-order/src/test/java/com/training/inno/ms/msorder/MsOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.inno.ms.msorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
